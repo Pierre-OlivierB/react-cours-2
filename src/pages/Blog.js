c08e2f2ceaf49f6d48ea68629fa0ff6a42f8ab40
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-12-07 22:45:53 +0100

    add crud nosql

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,18 +1,38 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
+import Article from "../components/Article";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
 const Blog = () => {
+  const [blogData, setBlogData] = useState([]);
+  const [author, setAuthor] = useState("");
   const [content, setContent] = useState("");
   const [error, setError] = useState(false);
+
+  const getData = () => {
+    axios
+      .get("http://localhost:3004/articles")
+      .then((res) => setBlogData(res.data));
+  };
+  useEffect(() => getData(), []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (content.length < 140) {
       setError(true);
     } else {
+      axios.post("http://localhost:3004/articles", {
+        author,
+        content,
+        date: Date.now(),
+      });
       setError(false);
+      setAuthor("");
+      setContent("");
+      getData();
     }
   };
   return (
@@ -22,16 +42,28 @@ const Blog = () => {
       <h1>Blog</h1>
 
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="Nom" />
+        <input
+          type="text"
+          placeholder="Nom"
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+        />
         <textarea
           style={{ border: error ? "1px solid red" : "1px solid #61dafb" }}
           placeholder="Message"
           onChange={(e) => setContent(e.target.value)}
+          value={content}
         ></textarea>
         {error && <p>Veuillez écrire un min de 100 carac</p>}
         <input type="submit" value="Envoyer" />
       </form>
-      <ul></ul>
+      <ul>
+        {blogData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+      </ul>
     </div>
   );
 };
